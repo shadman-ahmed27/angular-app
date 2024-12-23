c318385b70174b1dad2b56445433c1f5b2854d37
Shadman Ahmed <shadifmg@gmail.com>
2024-12-22 22:26:19 -0600

    feat: form data block only shows after hitting submit button

--- src/app/first-comp/first-comp.component.ts
+++ src/app/first-comp/first-comp.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-first-comp',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   templateUrl: './first-comp.component.html',
-  styleUrl: './first-comp.component.scss',
+  styleUrls: ['./first-comp.component.scss'],
 })
-export class FirstCompComponent {}
+export class FirstCompComponent {
+  name: string = '';
+  email: string = '';
+  message: string = '';
+  isSubmitted: boolean = false;
+
+  onSubmit(): void {
+    this.isSubmitted = true;
+  }
+}
