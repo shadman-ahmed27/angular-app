43064d0010917916ea1c7b195b738ffdb7a77f59
Shadman Ahmed <shadifmg@gmail.com>
2024-12-22 23:07:13 -0600

    feat: created message details component. gave it styles. only shows when you hit submit. does not populate values yet

--- src/app/first-comp/first-comp.component.ts
+++ src/app/first-comp/first-comp.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { MessageDetailsComponent } from '../message-details/message-details.component';
 
 @Component({
   selector: 'app-first-comp',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, MessageDetailsComponent],
   templateUrl: './first-comp.component.html',
   styleUrls: ['./first-comp.component.scss'],
 })
@@ -14,8 +15,15 @@ export class FirstCompComponent {
   email: string = '';
   message: string = '';
   isSubmitted: boolean = false;
+  messages: Array<any> = [];
 
   onSubmit(): void {
     this.isSubmitted = true;
+    this.messages.push({
+      name: this.name,
+      email: this.email,
+      message: this.message,
+    });
+    console.log(this.messages);
   }
 }
