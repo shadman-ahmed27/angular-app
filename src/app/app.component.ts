485c235f1021c64b78e54e3377833efdc2515b21
Shadman Ahmed <shadifmg@gmail.com>
2024-12-22 19:05:33 -0600

    created new component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'shut up bitch fuck';
+  title = "Shozin's Forum";
 }
---
+++ src/app/first-comp/first-comp.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-first-comp',
+  imports: [],
+  templateUrl: './first-comp.component.html',
+  styleUrl: './first-comp.component.scss'
+})
+export class FirstCompComponent {
+
+}
